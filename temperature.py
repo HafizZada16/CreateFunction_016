3ad049625a135799a071ac6f2127481d71bc6794
HafizZada16 <hafizzada16@gmail.com>
2024-10-21 10:48:52 +0700

    fix

--- temperature.py
+++ temperature.py
@@ -4,16 +4,16 @@ def konversi_suhu(temp, unit):
     elif unit.upper() == 'F':
         return (temp - 32) * 5/9  # Fahrenheit to Celsius
     else:
-        raise ValueError("Unit tidak valid. Gunakan 'C' atau 'F'.")
+        return None
 
 # Input dari user
 temp = float(input("Masukkan nilai suhu: "))
 unit = input("Masukkan unit suhu ('C' untuk Celsius, 'F' untuk Fahrenheit): ")
 
 # Menghitung dan mencetak hasil konversi
-try:
-    hasil = konversi_suhu(temp, unit)
+hasil = konversi_suhu(temp, unit)
+if hasil is not None:
     print(f"Hasil konversi: {hasil:.2f} {'F' if unit.upper() == 'C' else 'C'}")
-except ValueError as e:
-    print(e)
+else:
+    print("Unit tidak valid. Gunakan 'C' atau 'F'.")
 
